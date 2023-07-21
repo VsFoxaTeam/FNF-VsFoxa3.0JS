de174a2b94f1b501bd5fb94b33426e1c1025dd6c
CharlesCatYT <sptubea@gmail.com>
2023-07-21 19:53:59 -0400

    Update Quick SETUP.bat

--- Quick SETUP.bat
+++ Quick SETUP.bat
@@ -1,9 +1,9 @@
 @echo off
-echo Quickly setting up JS Engine for compiling...
+echo Quickly setting up Vs. Foxa for compiling...
 haxelib --global install hmm
 haxelib --global run hmm install
-echo Finished. You may now compile JS Engine!
-echo Oh wait!! Just a little precaution..
+echo Finished. You may now compile Vs. Foxa!
+echo Oh wait!! Just a little precaution...
 haxelib install hxCodec 2.5.1
 haxelib set hxCodec 2.5.1
 echo There. NOW we're done.
