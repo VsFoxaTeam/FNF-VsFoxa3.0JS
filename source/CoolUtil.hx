71f11edc34509812bb6446d49ac6a65cb9442678
CharlesCatYT <sptubea@gmail.com>
2023-07-21 19:01:58 -0400

    Update CoolUtil.hx

--- source/CoolUtil.hx
+++ source/CoolUtil.hx
@@ -18,16 +18,13 @@ using StringTools;
 
 class CoolUtil
 {
-	public static var defaultDifficulties:Array<String> = [
-		'Easy',
-		'Normal',
-		'Hard'
-	];
-	public static var defaultDifficulty:String = 'Normal'; //The chart that has no suffix and starting difficulty on Freeplay/Story Mode
+	public static var defaultDifficulties:Array<String> = ['Easy', 'Normal', 'Hard'];
+	public static var defaultDifficulty:String = 'Normal'; // The chart that has no suffix and starting difficulty on Freeplay/Story Mode
 
 	public static var difficulties:Array<String> = [];
 
-	inline public static function quantize(f:Float, snap:Float){
+	inline public static function quantize(f:Float, snap:Float)
+	{
 		// changed so this actually works lol
 		var m:Float = Math.fround(f * snap);
 		trace(snap);
@@ -35,31 +32,42 @@ class CoolUtil
 	}
 
 	public static function formatString(string:String):String
+	{
+		var split:Array<String> = string.split('-');
+		var formattedString:String = '';
+		for (i in 0...split.length)
 		{
-			 var split:Array<String> = string.split('-');
-			 var formattedString:String = '';
-			 for (i in 0...split.length) 
-			 {
-				  var piece:String = split[i];
-				  var allSplit = piece.split('');
-				  var firstLetterUpperCased = allSplit[0].toUpperCase();
-				  var substring = piece.substr(1, piece.length - 1);
-				  var newPiece = firstLetterUpperCased + substring;
-				  if (i != split.length - 1)
-				  {
-						newPiece += " ";
-				  }
-				  formattedString += newPiece;
-			 }
-			 return formattedString;
+			var piece:String = split[i];
+			var allSplit = piece.split('');
+			var firstLetterUpperCased = allSplit[0].toUpperCase();
+			var substring = piece.substr(1, piece.length - 1);
+			var newPiece = firstLetterUpperCased + substring;
+			if (i != split.length - 1)
+			{
+				newPiece += " ";
+			}
+			formattedString += newPiece;
 		}
-	
+		return formattedString;
+	}
+
+	public static function getIntArray(max:Int):Array<Int>
+	{
+		var arr:Array<Int> = [];
+		for (i in 0...max)
+		{
+			arr.push(i);
+		}
+		return arr;
+	}
+
 	public static function getDifficultyFilePath(num:Null<Int> = null)
 	{
-		if(num == null) num = PlayState.storyDifficulty;
+		if (num == null)
+			num = PlayState.storyDifficulty;
 
 		var fileSuffix:String = difficulties[num];
-		if(fileSuffix != defaultDifficulty)
+		if (fileSuffix != defaultDifficulty)
 		{
 			fileSuffix = '-' + fileSuffix;
 		}
@@ -75,11 +83,13 @@ class CoolUtil
 		return difficulties[PlayState.storyDifficulty].toUpperCase();
 	}
 
-	inline public static function boundTo(value:Float, min:Float, max:Float):Float {
+	inline public static function boundTo(value:Float, min:Float, max:Float):Float
+	{
 		return Math.max(min, Math.min(max, value));
 	}
 
-	inline public static function clamp(value:Float, min:Float, max:Float):Float {
+	inline public static function clamp(value:Float, min:Float, max:Float):Float
+	{
 		return Math.max(min, Math.min(max, value));
 	}
 
@@ -87,9 +97,11 @@ class CoolUtil
 	{
 		var daList:Array<String> = [];
 		#if sys
-		if(FileSystem.exists(path)) daList = File.getContent(path).trim().split('\n');
+		if (FileSystem.exists(path))
+			daList = File.getContent(path).trim().split('\n');
 		#else
-		if(Assets.exists(path)) daList = Assets.getText(path).trim().split('\n');
+		if (Assets.exists(path))
+			daList = Assets.getText(path).trim().split('\n');
 		#end
 
 		for (i in 0...daList.length)
@@ -99,6 +111,7 @@ class CoolUtil
 
 		return daList;
 	}
+
 	public static function listFromString(string:String):Array<String>
 	{
 		var daList:Array<String> = [];
@@ -111,25 +124,35 @@ class CoolUtil
 
 		return daList;
 	}
-	public static function dominantColor(sprite:flixel.FlxSprite):Int{
+
+	public static function dominantColor(sprite:flixel.FlxSprite):Int
+	{
 		var countByColor:Map<Int, Int> = [];
-		for(col in 0...sprite.frameWidth){
-			for(row in 0...sprite.frameHeight){
-			  var colorOfThisPixel:Int = sprite.pixels.getPixel32(col, row);
-			  if(colorOfThisPixel != 0){
-				  if(countByColor.exists(colorOfThisPixel)){
-				    countByColor[colorOfThisPixel] =  countByColor[colorOfThisPixel] + 1;
-				  }else if(countByColor[colorOfThisPixel] != 13520687 - (2*13520687)){
-					 countByColor[colorOfThisPixel] = 1;
-				  }
-			  }
+		for (col in 0...sprite.frameWidth)
+		{
+			for (row in 0...sprite.frameHeight)
+			{
+				var colorOfThisPixel:Int = sprite.pixels.getPixel32(col, row);
+				if (colorOfThisPixel != 0)
+				{
+					if (countByColor.exists(colorOfThisPixel))
+					{
+						countByColor[colorOfThisPixel] = countByColor[colorOfThisPixel] + 1;
+					}
+					else if (countByColor[colorOfThisPixel] != 13520687 - (2 * 13520687))
+					{
+						countByColor[colorOfThisPixel] = 1;
+					}
+				}
 			}
-		 }
+		}
 		var maxCount = 0;
-		var maxKey:Int = 0;//after the loop this will store the max color
+		var maxKey:Int = 0; // after the loop this will store the max color
 		countByColor[flixel.util.FlxColor.BLACK] = 0;
-			for(key in countByColor.keys()){
-			if(countByColor[key] >= maxCount){
+		for (key in countByColor.keys())
+		{
+			if (countByColor[key] >= maxCount)
+			{
 				maxCount = countByColor[key];
 				maxKey = key;
 			}
@@ -145,7 +168,8 @@ class CoolUtil
 
 		@author Leather128
 	**/
-	public static function coolError(message:Null<String> = null, title:Null<String> = null):Void {
+	public static function coolError(message:Null<String> = null, title:Null<String> = null):Void
+	{
 		#if !linux
 		lime.app.Application.current.window.alert(message, title);
 		#else
@@ -162,14 +186,14 @@ class CoolUtil
 		FlxG.state.add(text);
 
 		FlxTween.tween(text, {alpha: 0, y: 8}, 5, {
-			onComplete: function(_) {
+			onComplete: function(_)
+			{
 				FlxG.state.remove(text);
 				text.destroy();
 			}
 		});
 		#end
 	}
-
 
 	public static function numberArray(max:Int, ?min = 0):Array<Int>
 	{
@@ -181,16 +205,19 @@ class CoolUtil
 		return dumbArray;
 	}
 
-	//uhhhh does this even work at all? i'm starting to doubt
-	public static function precacheSound(sound:String, ?library:String = null):Void {
+	// uhhhh does this even work at all? i'm starting to doubt
+	public static function precacheSound(sound:String, ?library:String = null):Void
+	{
 		Paths.sound(sound, library);
 	}
 
-	public static function precacheMusic(sound:String, ?library:String = null):Void {
+	public static function precacheMusic(sound:String, ?library:String = null):Void
+	{
 		Paths.music(sound, library);
 	}
 
-	public static function browserLoad(site:String) {
+	public static function browserLoad(site:String)
+	{
 		#if linux
 		Sys.command('/usr/bin/xdg-open', [site]);
 		#else
@@ -203,7 +230,8 @@ class CoolUtil
 		so Base Psych saves won't conflict with yours
 		@BeastlyGabi
 	**/
-	public static function getSavePath(folder:String = 'ShadowMario'):String {
+	public static function getSavePath(folder:String = 'ShadowMario'):String
+	{
 		@:privateAccess
 		return #if (flixel < "5.0.0") folder #else FlxG.stage.application.meta.get('company')
 			+ '/'
